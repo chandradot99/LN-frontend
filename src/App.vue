56a7bbbdeb73de24a376e31bf5a0e4c8943ba073
Chandra Shekhar <shekharait254@gmail.com>
2021-01-25 14:04:47 +0530

    1. Add icons sprite files 2. Add Shared Header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-100">
+    <ln-header></ln-header>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import LNHeader from './components/shared/ln-header'
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
+    'ln-header': LNHeader
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+  .ln-header {
+    background-color: #0E0E0E;
+    height: 47px;
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    .ln-logo {
+      width: 150px;
+    }
+    .ln-links {
+      ul {
+        padding-left: 0;
+        li {
+          list-style: none;
+          display: inline-flex;
+          align-items: center;
+          margin: 0 10px;
+          .icon {
+            width: 21px;
+            height: 21px;
+          }
+          span {
+            padding-left: 10px;
+            color: #ffffff;
+            font-size: 15px;
+          }
+          &.active {
+            .icon {
+              fill: orange
+            }
+            span {
+              color: orange;
+            }
+          }
+        }
+      }
+    }
+  }
 </style>
+
+<style lang="scss">
+  @import url('https://fonts.googleapis.com/css2?family=Roboto');
+
+  body {
+    margin: 0;
+    height: 100vw;
+    font-family: 'Roboto', sans-serif;
+    background-color: #282828;
+  }
+  .h-100 {
+    height: 100%;
+  }
+</style>
+
---
+++ src/components/shared/ln-header.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="ln-header">
+    <div class="ln-logo">
+      <img src="/assets/img/Logo.svg" alt="">
+    </div>
+    <div class="ln-links">
+      <ul>
+        <li>
+          <svg class="icon">
+            <use xlink:href="/assets/img/ln-icons.svg#ico_dashboard"></use>
+          </svg>
+          <span>Dashboard</span>
+        </li>
+        <li class="active">
+          <svg class="icon">
+            <use xlink:href="/assets/img/ln-icons.svg#ico_users"></use>
+          </svg>
+          <span>Users</span>
+        </li>
+        <li>
+          <svg class="icon">
+            <use xlink:href="/assets/img/ln-icons.svg#ico_sessionmanager"></use>
+          </svg>
+          <span>Session Manager</span>
+        </li>
+      </ul>
+    </div>
+    <div class="right-nav">
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="scss" scoped>
+  .ln-header {
+    background-color: #282828;
+    height: 47px;
+    display: flex;
+    align-items: center;
+    padding: 10px 20px;
+    .ln-logo {
+      width: 150px;
+    }
+    .ln-links {
+      ul {
+        padding-left: 0;
+        li {
+          list-style: none;
+          display: inline-flex;
+          align-items: center;
+          margin: 0 10px;
+          .icon {
+            width: 21px;
+            height: 21px;
+          }
+          span {
+            padding-left: 10px;
+            color: #ffffff;
+            font-size: 15px;
+          }
+          &.active {
+            .icon {
+              fill: orange
+            }
+            span {
+              color: orange;
+            }
+          }
+        }
+      }
+    }
+  }
+</style>
