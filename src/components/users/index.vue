54af367c193466ee669174a829086d50a08a1aab
Chandra Shekhar <shekharait254@gmail.com>
2021-01-25 15:11:05 +0530

    add bootstrap vue module

--- src/components/users/index.vue
+++ src/components/users/index.vue
@@ -1,6 +1,19 @@
 <template>
   <div class="users-wrap h-100">
-    Users
+    <div class="header-section">
+      <div>
+        <svg class="icon">
+          <use xlink:href="/assets/img/ln-icons.svg#ico_users"></use>
+        </svg>
+        <span>Users</span>
+      </div>
+      <div>
+        <b-input></b-input>
+      </div>
+    </div>
+    <div class="list-section">
+
+    </div>
   </div>
 </template>
 
